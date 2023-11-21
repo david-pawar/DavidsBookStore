bdec73b91699bff213277c170b0ffe02a00db688
david-pawar <135388636+david-pawar@users.noreply.github.com>
2023-11-21 05:57:28 -0500

    Created coverType.cs, ICoverTypeRespositoy.cs and CoverTypeRepository.cs, there are some issues regarding accessibility

---
+++ DavidsBooks.Models/CoverType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DavidsBooks.Models
+{
+    public class CoverType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
